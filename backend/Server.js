42703f396a1ff0df542341a5a0e362f89875f8a9
Ashutosh Mishra <ashutoshmishra61679@gmail.com>
2024-05-25 00:46:16 +0530

    Request can be made from any location

--- backend/Server.js
+++ backend/Server.js
@@ -5,7 +5,6 @@ const routes = require('./routes/ToDoRoutes')
 const cors= require("cors")
 const app = express();
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend origin
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
